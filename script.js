01ad7a2e679725382fd0da3bff9e2bab250470fa
silentruntime <silentruntime@protonmail.com>
2025-05-07 15:58:36 -0400

    modules to import previouslyList

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//import { previouslyList } from './previouslylist.js';
-//let previously = previouslyList;
+import { previouslyList } from "./previouslylist.js";
 
 let apiKey = "W7gIJGjUUnOV3a5Msp8VcyIU02AWiXz7";
 //let apiKey = "ldD9shrU9AywvAcnn5IOs8QaHWgvfUvv"
@@ -9,84 +8,7 @@ let search = document.getElementById("search");
 let searchAuthor = document.getElementById("author");
 let searchTitle = document.getElementById("title");
 let clearBtn = document.getElementById("clear");
-let nytimesBestSellers;
 
-let previously = [
-  { title: "The Pelican Brief", author: "John Grisham" },
-  { title: "Eat, Pray, Love", author: "Elizabeth Gilbert" },
-  { title: "Twilight", author: "Stephenie Meyer" },
-  { title: "The Shack", author: "William P Young" },
-  { title: "Crazy House", author: "James Patterson and Gabrielle Charbonnet" },
-  { title: "The Secret", author: "Rhonda Byrne" },
-  { title: "The Da Vinci Code", author: "Dan Brown" },
-  { title: "Malice", author: "Danielle Steel" },
-  { title: "Fourth Wing", author: "Rebecca Yarros" },
-  { title: "Hillbilly Elegy", author: "JD Vance" },
-  { title: "The Housemaid", author: "Freida McFadden" },
-  { title: "Lean In", author: "Sheryl Sandberg" },
-  { title: "A Court of Thorns and Roses", author: "Sarah J. Maas" },
-  { title: "The House in the Pines", author: "Ana Reyes" },
-  { title: "Memoirs of a Geisha", author: "Arthur Golden" },
-  { title: "While Justice Sleeps", author: "Stacey Abrams" },
-  { title: "Happy Place", author: "Emily Henry" },
-  { title: "Divine Rivals", author: "Rebecca Ross" },
-  { title: "Just the Nicest Couple", author: "Mary Kubica" },
-  { title: "The Paper Palace", author: "Miranda Cowley Heller" },
-  { title: "A Man Called Ove", author: "Fredrik Backman" },
-  { title: "The Bodyguard", author: "Katherine Center" },
-  { title: "Eleanor Oliphant Is Completely Fine", author: "Gail Honeyman" },
-  { title: "Dichotomy of Leadership", author: "Jocko Willink and Leif Babin" },
-  { title: "Verity", author: "Colleen Hoover" },
-  { title: "Cherry Cheesecake Murder", author: "Joanne Fluke" },
-  { title: "1984", author: "George Orwell" },
-  { title: "Before We Were Yours", author: "Lisa Wingate" },
-  { title: "Klara and the Sun", author: "Kazuo Ishiguro" },
-  { title: "Think and Grow Rich", author: "Napoleon Hill" },
-  { title: "World War Z", author: "Max Brooks" },
-  { title: "In Five Years", author: "Rebecca Serle" },
-  { title: "You", author: "Caroline Kepnes" },
-  { title: "You Are a Badass at Making Money", author: "Jen Sincero" },
-  { title: "The Sun Down Motel", author: "Simone St. James" },
-  { title: "It's Not About the Bike", author: "Lance Armstrong" },
-  { title: "The Grace Year", author: "Kim Liggett" },
-  { title: "Breakfast of Champions", author: "Kurt Vonnegut" },
-  { title: "My Dark Vanessa", author: "Kate Elizabeth Russell" },
-  { title: "Bringing Up Bébé", author: "Pamela Druckerman" },
-  { title: "Dark Matter", author: "Blake Crouch" },
-  { title: "The Whisper Man", author: "Alex North" },
-  { title: "Survive the Night", author: "Riley Sager" },
-  { title: "Big Magic", author: "Elizabeth Gilbert" },
-  { title: "The Wife Upstairs", author: "Rachel Hawkins" },
-  { title: "The Subtle Art of Not Giving a F*ck", author: "Mark Manson" },
-  { title: "The Vanishing Half", author: "Brit Bennett" },
-  { title: "Peyton Place", author: "Grace Metalious" },
-  { title: "The Wives", author: "Tarryn Fisher" },
-  { title: "Bridgerton", author: "Julia Quinn" },
-  { title: "The Old Man and the Sea", author: "Ernest Hemingway" },
-  { title: "Untamed", author: "Glennon Doyle" },
-  { title: "The Overachievers", author: "Alexandra Robbins" },
-  { title: "Open Book", author: "Jessica Simpson" },
-  { title: "The Guest List", author: "Lucy Foley" },
-  { title: "This Man Confessed", author: "Jodi Ellen Malpas" },
-  { title: "Nine Perfect Strangers", author: "Liane Moriarty" },
-  { title: "The Christmas Shoes", author: "Donna VanLiere" },
-  {
-    title: "Gossip Girl: All I Want Is Everything",
-    author: "Cecily von Ziegesar",
-  },
-  { title: "Gerald's Game", author: "Stephen King" },
-  { title: "Neon Prey", author: "John Sandford" },
-  { title: "Artemis Fowl", author: "Eoin Colfer" },
-  { title: "Naked Came the Stranger", author: "Penelope Ashe" },
-  { title: "Women Who Work", author: "Ivanka Trump" },
-  { title: "Naughty Neighbor", author: "Janet Evanovich" },
-  { title: "Lolita", author: "Vladimir Nabokov" },
-  { title: "American Dirt", author: "Jeanine Cummins" },
-  { title: "Valley of the Dolls", author: "Jacqueline Susann" },
-  { title: "The Christmas Train", author: "David Baldacci" },
-  { title: "Relentless", author: "Dean Koontz" },
-  { title: "L.A. Candy", author: "Lauren Conrad" },
-];
 /*
 apikeyinput.addEventListener("change", () => {
   apiKey = apikeyinput.value
@@ -258,12 +180,16 @@ clearBtn.addEventListener("click", (e) => {
 
 const previouslyOn = () => {
   document.getElementById("books").innerHTML = "Previously on Mean Book Club:";
-  for (let book of previously) {
+  for (let book of previouslyList) {
     let newLink = document.createElement("a");
     newLink.onclick = () => searchFor(`${book.author}`, `${book.title}`);
     newLink.innerText = `${book.title} by ${book.author}`;
     document.getElementById("books").appendChild(newLink);
   }
 };
+
+window.searchFor = searchFor;
+window.getDetails = getDetails;
+window.previouslyOn = previouslyOn;
 
 previouslyOn();
