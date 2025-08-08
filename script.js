e31421f2e09b164285cd1dc94f52664d1c2326e2
pkdvalis <github.com.clip993@passinbox.com>
2025-08-08 14:53:29 -0400

    added titleCase()

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ const allBooks = [];
 let filesLoaded = false;
 const ONE_MONTH_AGO = Date.now() - 2629800000;
 
+function titleCase(str) {
+  str = str.toLowerCase();
+  str = str.split(" ");
+
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+
+  return str.join(" ");
+}
+
 async function loadFiles(author = "", title = "", isbn = false) {
   if (filesLoaded && (author || title || isbn)) {
     searchJSON(author, title, isbn);
@@ -222,6 +233,7 @@ const displaySearchResults = (results, isbn = false) => {
 
     book.contributor = cleanAuthorBy(book.contributor);
     //Basic Info
+    book.title = titleCase(book.title);
     let listing = `
           <div class="entry">
             <div class="content">
